533b657139a20defa44bcae307827f4d6555dfd9
DEWWA06 <devnithamethnodh@gmail.com>
2026-09-05 02:18:22 +0530

    Add WBS class

---
+++ src/main/java/edu/curtin/app/WBS.java
@@ -0,0 +1,40 @@
+package edu.curtin.app;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class WBS 
+{
+    private Map<String, Task> tasks;
+    private List<Task> roots;
+
+    public WBS()
+    {
+        tasks = new HashMap<>();
+        roots = new ArrayList<>();
+    }
+    
+    public void addTask(Task task)
+    {
+        tasks.put(task.getId(), task);
+    }
+
+    public Task getTask(String id)
+    {
+        return tasks.get(id);
+    }
+
+    public void addRoot(Task task)
+    {
+        roots.add(task);
+    }
+
+    public List<Task> getRoots()
+    {
+        return roots;
+    }
+}
+
+
